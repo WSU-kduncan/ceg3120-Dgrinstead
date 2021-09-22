32257634e2e9cb6e0d8f94e5c8f524191d1ea4e0
Dionte Grinstead <grinsead.11@wright.edu>
2021-09-22 02:37:41 +0000

    go

--- Discord-Bot/Bot.py
+++ Discord-Bot/Bot.py
@@ -29,25 +29,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
+    cybersecurity_quotes = [
+        'Technology trust is a good thing, but control is a better one',
+        'Stephane Nappo',
         (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
+            'If security were all that mattered, computers would never be turned on, let alone hooked into' 
+           ' a network with literally millions of potential intruders'
         ),
     ]
 
-    hitchhiker_quotes = [
-        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-        'It is a mistake to think you can solve any major problems just with potatoes.',
-        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+    security_quotes = [
+        'Security used to be an inconvenience sometimes, but now its a necessity all the time'
     ]
 
-    if message.content == 'towel!':
-        #response = random.choice(brooklyn_99_quotes)
-        response = random.choice(hitchhiker_quotes)
+    if message.content == 'whats up':
+        #response = random.choice(cybersecurity_quotes)
+        response = random.choice(security_quotes)
         await message.channel.send(response)
 
 client.run(TOKEN)
